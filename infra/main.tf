6ddfdda13d59fff080936b170cd61d3b97c45822
Santhosh S <hsohtnas001@gmail.com>
2023-04-04 19:21:29 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,10 +3,6 @@ variable "sample" {
   default = "Sample"
 }
 
-data "external" "example" {
-  program = ["aws", "iam", "list-policies"]
-}
-
 output "sample" {
   value = [for policy in data.external.example.result.Policies : policy.PolicyName]
 }
